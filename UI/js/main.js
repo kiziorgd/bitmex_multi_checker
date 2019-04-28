db2b05ef11d76876302c63a1c05c40fec8305d59
Greg Dabrowski <gregory.dabrowski@gmail.com>
2019-04-28 19:00:58 +0200

    fix data handling

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    function statsToBtcFormat(sats){
+    function satsToBtcFormat(sats){
         return `${(sats/100000000).toFixed(8)} ฿`
     }
 
@@ -19,10 +19,10 @@ $(function() {
                     type: 'POST',
                     contentType: 'application/json',
                     url: 'https://tadek.tele.com.pl/bitmex-multichecker/users/data',
+//                    url: 'http://127.0.0.1:8000/users/data',
                     data: e.target.result,
                     dataType: 'json',
                 }).done(function(result){
-                    console.log(result)
                     let inc = 0;
                     result.forEach(function(el){
                         inc++;
@@ -30,11 +30,11 @@ $(function() {
 
                         row.append('<th scope="row">'+inc+'</th>')
                         row.append('<td>'+el.username+'</td>')
-                        row.append('<td>'+statsToBtcFormat(el.balance)+'</td>')
+                        row.append('<td>'+satsToBtcFormat(el.balance)+'</td>')
                         row.append('<td>'+el.position+' contracts</td>')
                         row.append('<td>'+el.sellOrders+' contracts</td>')
-                        row.append('<td>'+statsToBtcFormat(el.deposited)+'</td>')
-                        row.append('<td>'+statsToBtcFormat(el.withdrawn)+'</td>')
+                        row.append('<td>'+satsToBtcFormat(el.deposited)+'</td>')
+                        row.append('<td>'+satsToBtcFormat(el.withdrawn)+'</td>')
                         row.append('<td>'+el.referer+'</td>')
 
                       $('#table_body').append(row)
